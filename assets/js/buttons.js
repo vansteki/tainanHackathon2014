ea6ae0c16acc6a412f35dcf1ee3abc0f7545da2c
Tonypai <tony77794@gmail.com>
2014-06-21 17:58:26 +0800

    combine index and datalist

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -1,73 +1,92 @@
 $(function() {
-$("form").submit(function(e){ return false; });
+    app.init();
+    
+    $("form").submit(function(e){ return false; });
 
-var iniBasicUI = function() {
-    var $foodBtn = $('#food');
-    var $spotBtn = $('#spot');
-    var $convBtn = $('#conv');
-    var $subClass = $('#sub-class');
-    var $searchForm = $('#search-form');
-    var $navLink = $('.navLink');
+    $("#sub-class").change(function () {
+        $("#main").text("");
+        switch($(this).find("option:selected").prop("value")) {
+            case "dining": app.food.dining(); break;
+            case "restaurant": app.food.restaurant(); break;
+            case "nightmarket": app.food.nightmarket(); break;
+        }
+    });
+    
+    var iniBasicUI = function() {
+        var $foodBtn = $('#food');
+        var $spotBtn = $('#spot');
+        var $convBtn = $('#conv');
+        var $subClass = $('#sub-class');
+        var $searchForm = $('#search-form');
+        var $navLink = $('#home');
+        var $main = $('#main');
 
-    $foodBtn.click(function() {
-        switchPage();
-        $subClass.html('');
-        $subClass.prev().text('店家');
-        $subClass.append($('<option>').text('店家'));
-        $subClass.append($('<option>').text('餐廳'));
-        $subClass.append($('<option>').text('夜市'));
-    });
-    $spotBtn.click(function() {
-        switchPage();
-        $subClass.html('');
-        $subClass.prev().text('古蹟');
-        $subClass.append($('<option>').text('古蹟'));
-        $subClass.append($('<option>').text('寺廟'));
-    });
-    $convBtn.click(function() {
-        switchPage();
-        $subClass.html('');
-        $subClass.prev().text('廁所');
-        $subClass.append($('<option>').text('廁所'));
-        $subClass.append($('<option>').text('Wifi '));
-    });
-    $navLink.click(function() {
-       showAllClassBtn();
-       hideNavLink();
-       hideSearchForm();
-    });
+        $foodBtn.click(function() {
+            switchPage();
+            $subClass.html('');
+            $subClass.prev().text('店家');
+            $subClass.append($('<option>').text('店家').prop("value", "dining"));
+            $subClass.append($('<option>').text('餐廳').prop("value", "restaurant"));
+            $subClass.append($('<option>').text('夜市').prop("value", "nightmarket"));
+        });
+        $spotBtn.click(function() {
+            switchPage();
+            $subClass.html('');
+            $subClass.prev().text('古蹟');
+            $subClass.append($('<option>').text('古蹟'));
+            $subClass.append($('<option>').text('寺廟'));
+        });
+        $convBtn.click(function() {
+            switchPage();
+            $subClass.html('');
+            $subClass.prev().text('廁所');
+            $subClass.append($('<option>').text('廁所'));
+            $subClass.append($('<option>').text('Wifi '));
+        });
+        $navLink.click(function() {
+           showAllClassBtn();
+           hideMain();
+           hideSearchForm();
+        });
 
-    var showNavLink = function() {
-        $navLink.removeClass('hidden');
+        var showNavLink = function() {
+            $navLink.removeClass('hidden');
+        };
+        var hideNavLink = function() {
+            $navLink.addClass('hidden');
+        };
+        var showMain = function () {
+            $main.removeClass('hidden');
+        };
+        var hideMain = function () {
+            $main.addClass('hidden');
+        };
+        var showSearchForm = function() {
+            $searchForm.removeClass('hidden');
+        };
+        var hideSearchForm = function() {
+            $searchForm.addClass('hidden');
+        };
+        var showAllClassBtn = function() {
+            $foodBtn.removeClass('hidden'); 
+            $spotBtn.removeClass('hidden'); 
+            $convBtn.removeClass('hidden');
+        };
+        var hideAllClassBtn = function() {
+            $foodBtn.addClass('hidden');
+            $spotBtn.addClass('hidden');
+            $convBtn.addClass('hidden');    
+        };
+        var switchPage = function() {
+            $foodBtn.addClass('hidden');
+            $spotBtn.addClass('hidden');
+            $convBtn.addClass('hidden');
+            $main.addClass('hidden');
+            showSearchForm();
+            showNavLink();
+            showMain();
+            $navLink.find('#prev-link').removeClass('ui-btn-active'); //remove active blue color
+        };
     };
-    var hideNavLink = function() {
-        $navLink.addClass('hidden');
-    }
-    var showSearchForm = function() {
-        $searchForm.removeClass('hidden');
-    };
-    var hideSearchForm = function() {
-        $searchForm.addClass('hidden');
-    };
-    var showAllClassBtn = function() {
-        $foodBtn.removeClass('hidden'); 
-        $spotBtn.removeClass('hidden'); 
-        $convBtn.removeClass('hidden');
-    };
-    var hideAllClassBtn = function() {
-        $foodBtn.addClass('hidden');
-        $spotBtn.addClass('hidden');
-        $convBtn.addClass('hidden');    
-    };
-    var switchPage = function() {
-        $foodBtn.addClass('hidden');
-        $spotBtn.addClass('hidden');
-        $convBtn.addClass('hidden');
-        showSearchForm();
-        showNavLink();
-        $navLink.find('#prev-link').removeClass('ui-btn-active'); //remove active blue color
-    };
-};
-iniBasicUI();
-
-}); //end of jQuery done
+    iniBasicUI();
+});
--- assets/js/data.js
+++ assets/js/data.js
@@ -1,91 +1,100 @@
 var app = {
+	init: function () {
+		var root = this;
+		var data = {
+			"美食": {
+				"店家": "assets/json/food_dining.json",
+            	"餐廳": "http://data.tainan.gov.tw/api/action/datastore_search?resource_id=a4dda55a-6ffb-4423-b968-471de89c779f&limit=100",
+            	"夜市": "assets/json/food_nightmarket.json"
+            },
+            "景點": {},
+            "便民": {}
+        };
+
+        var requestQueue = [];
+		var getjson = function (url, id) {
+			requestQueue.push($.getJSON(url, function(data) { window[id] = data; }));
+		};
+
+		getjson(data["美食"]["店家"], "dining");
+		getjson(data["美食"]["餐廳"], "restaurant");
+		getjson(data["美食"]["夜市"], "nightmarket");
+
+		$.when.apply($, requestQueue).then(function(data) {
+			console.log("Test")
+			root.food.dining();
+		});
+
+	},
 	food: {
 		dining: function () {
-			$.ajax({
-				type: "GET",
-				url: "assets/json/food_dining.json",
-				success: function(data) {
-					
-					var checkit = function (onoff) {
-						if(onoff == 1)
-							return 'O';
-						else 
-							return 'X';
-					};
+			var data = window["dining"];
+			var checkit = function (onoff) {
+				if(onoff == 1)
+					return 'O';
+				else 
+					return 'X';
+			};
 
-					$("#main").append('<div id="dining" data-role="collapsible-set" data-filter="true" data-filter-placeholder="搜尋"></div>');
-					$("#dining").collapsibleset();
+			$("#main").append('<div id="dining" data-role="collapsible-set" data-filter="true" data-input="#search"></div>');
+			$("#dining").collapsibleset();
 
-					$.each(data, function(key, val) {
+			$.each(data, function(key, val) {
 
-						var bd = val["營業時間"].split("*");
-						var table = '<table class="ui-responsive ui-table ui-table-reflow">';
-						table += "<tr><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th><th>日</th>";
-						table += '<tr><td>'+checkit(bd[0])+'</td><td>'+checkit(bd[1])+'</td><td>'+checkit(bd[2])+'</td><td>'+checkit(bd[3])+'</td><td>'+checkit(bd[4])+'</td><td>'+checkit(bd[5])+'</td><td>'+checkit(bd[6])+'</td>';
-						table += '</table>';
+				var bd = val["營業時間"].split("*");
+				var table = '<table class="ui-responsive ui-table ui-table-reflow">';
+				table += "<tr><th>一</th><th>二</th><th>三</th><th>四</th><th>五</th><th>六</th><th>日</th>";
+				table += '<tr><td>'+checkit(bd[0])+'</td><td>'+checkit(bd[1])+'</td><td>'+checkit(bd[2])+'</td><td>'+checkit(bd[3])+'</td><td>'+checkit(bd[4])+'</td><td>'+checkit(bd[5])+'</td><td>'+checkit(bd[6])+'</td>';
+				table += '</table>';
 
-						$("#dining").append(
-							'<div data-role="collapsible">'+
-							'	<h3>'+val["餐飲店家名稱"]+'</h3>'+
-							'	<p>'+table+
-							'		營業時間：'+bd[7]+'<br />'+
-							'		店家地址：<a href="http://maps.google.com.tw/?q='+val["店家地址"]+'" target="_blank">'+val["店家地址"]+'</a><br />'+
-							'		店家電話：<a href="tel:'+val["店家電話"]+'">'+val["店家電話"]+'</a></p>'+
-							'	</div>'+
-							'</div>'
-						);
+				$("#dining").append(
+					'<div data-role="collapsible">'+
+					'	<h3>'+val["餐飲店家名稱"]+'</h3>'+
+					'	<p>'+table+
+					'		營業時間：'+bd[7]+'<br />'+
+					'		店家地址：<a href="http://maps.google.com.tw/?q='+val["店家地址"]+'" target="_blank">'+val["店家地址"]+'</a><br />'+
+					'		店家電話：<a href="tel:'+val["店家電話"]+'">'+val["店家電話"]+'</a></p>'+
+					'	</div>'+
+					'</div>'
+				);
 
-						if(key == data.length - 1) {
-							$("#dining").collapsibleset("refresh");
-						}
-					})
+				if(key == data.length - 1) {
+					$("#dining").collapsibleset("refresh");
 				}
-			});
+			})
 		},
 		restaurant: function () {
-			$.ajax({
-				type: "GET",
-				url: "http://data.tainan.gov.tw/api/action/datastore_search?resource_id=a4dda55a-6ffb-4423-b968-471de89c779f&limit=100",
-				success: function(data) {
+			var data = window["restaurant"];
+			$("#main").append('<div id="restaurant" data-role="collapsible-set" data-filter="true" data-input="#search"></div');
+			$("#restaurant").collapsibleset();
 
-					$("#main").append('<div id="restaurant" data-role="collapsible-set" data-filter="true" data-filter-placeholder="搜尋"></div');
-					$("#restaurant").collapsibleset();
-
-					$.each(data.result.records, function(key, val) {
-						$("#restaurant").append(
-							'<div data-role="collapsible">'+
-							'	<h3>'+val["餐廳名稱"]+'</h3>'+
-							'	<p>店家地址：<a href="http://maps.google.com.tw/?q=台南市'+val["區別"]+val["地址"]+'" target="_blank">台南市'+val["區別"]+val["地址"]+'</a><br />'+
-							'		店家電話：<a href="tel:'+val["電話"]+'">'+val["電話"]+'</a></p>'+
-							'	</div>'+
-							'</div>'
-						)
-						if(key == data.result.records.length - 1) {
-							$("#restaurant").collapsibleset("refresh");
-						}
-					})
+			$.each(data.result.records, function(key, val) {
+				$("#restaurant").append(
+					'<div data-role="collapsible">'+
+					'	<h3>'+val["餐廳名稱"]+'</h3>'+
+					'	<p>店家地址：<a href="http://maps.google.com.tw/?q=台南市'+val["區別"]+val["地址"]+'" target="_blank">台南市'+val["區別"]+val["地址"]+'</a><br />'+
+					'		店家電話：<a href="tel:'+val["電話"]+'">'+val["電話"]+'</a></p>'+
+					'	</div>'+
+					'</div>'
+				)
+				if(key == data.result.records.length - 1) {
+					$("#restaurant").collapsibleset("refresh");
 				}
-			});
+			})
 		},
 		nightmarket: function () {
-			$.ajax({
-				type: "GET",
-				url: "assets/json/food_nightmarket.json",
-				success: function(data) {
-					
-					$("#main").append('<ul id="nightmarket" data-role="listview" data-filter="true" data-filter-placeholder="搜尋" data-inset="true"></ul>');
-					$("#nightmarket").listview();
-					
-					$.each(data, function(key, val) {
-						$("#nightmarket").append(
-							'<li><a href="'+val["地圖"]+'" target="_blank">'+val["區別"]+' - '+val["夜市名"]+'</a></li>'
-						)
-						if(key == data.length - 1) {
-							$("#nightmarket").listview("refresh");
-						}
-					})
+			var data = window["nightmarket"];
+			$("#main").append('<ul id="nightmarket" data-role="listview" data-filter="true" data-input="#search" data-inset="true"></ul>');
+			$("#nightmarket").listview();
+			
+			$.each(data, function(key, val) {
+				$("#nightmarket").append(
+					'<li><a href="'+val["地圖"]+'" target="_blank">'+val["區別"]+' - '+val["夜市名"]+'</a></li>'
+				)
+				if(key == data.length - 1) {
+					$("#nightmarket").listview("refresh");
 				}
-			});
+			})
 		}
 	}
 };
