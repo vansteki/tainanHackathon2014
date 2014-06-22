12d520ece2ff5438ae1b6385d71f86c586cdf075
Tonypai <tony77794@gmail.com>
2014-06-22 10:28:16 +0800

    history activated

--- assets/js/data.js
+++ assets/js/data.js
@@ -13,17 +13,17 @@ var app = {
 		var root = this;
 		var data = {
 			"美食": {
-				"店家": "http://localhost/food_dining.json",
-            	"餐廳": "http://localhost/food_restaurant.json",
-            	"夜市": "http://localhost/food_nightmarket.json"
+				"店家": "assets/json/food_dining.json",
+            	"餐廳": "assets/json/food_restaurant.json",
+            	"夜市": "assets/json/food_nightmarket.json"
             },
             "景點": {
-            	"古蹟": "http://localhost/spot_interestingPlace.json",
-            	"寺廟": "http://localhost/spot_temple.json"
+            	"古蹟": "assets/json/spot_interestingPlace.json",
+            	"寺廟": "assets/json/spot_temple.json"
             },
             "便民": {
             	"廁所": "http://data.tainan.gov.tw/api/action/datastore_search?resource_id=bde652b1-90d8-4b90-8ab4-f8bdc19242fc&limit=100",
-            	"Wifi": "http://localhost/service_wifi.json"
+            	"Wifi": "assets/json/service_wifi.json"
             }
         };
 
@@ -41,7 +41,6 @@ var app = {
 		getjson(data["便民"]["Wifi"], "wifi");
 
 		$.when.apply($, requestQueue).then(function(data) {
-			$('#home').removeClass("hidden");
 			$('#food').removeClass("hidden");
 			$('#spot').removeClass("hidden");
 			$('#conv').removeClass("hidden");
@@ -62,7 +61,7 @@ var app = {
 				
 				$("#dining").append(
 					'<div data-role="collapsible">'+
-					'	<h3>'+val["餐飲店家名稱"]+'</h3>'+//table+
+					'	<h3 id="dining-'+key+'">'+val["餐飲店家名稱"]+'</h3>'+
 					'	<p>營業時間：'+bd[7]+'<br />'+
 					'		店家地址：<a href="http://maps.google.com.tw/?q='+val["店家地址"]+'" target="_blank">'+val["店家地址"]+'</a><br />'+
 					'		店家電話：<a href="tel:'+val["店家電話"]+'">'+val["店家電話"]+'</a>'+
@@ -83,7 +82,7 @@ var app = {
 			$.each(data, function(key, val) { //data.result.records
 				$("#restaurant").append(
 					'<div data-role="collapsible">'+
-					'	<h3>'+val["餐廳名稱"]+'</h3>'+
+					'	<h3 id="restaurant-'+key+'">'+val["餐廳名稱"]+'</h3>'+
 					'	<p>店家地址：<a href="http://maps.google.com.tw/?q=台南市'+val["區別"]+val["地址"]+'" target="_blank">台南市'+val["區別"]+val["地址"]+'</a><br />'+
 					'	店家電話：<a href="tel:'+val["電話"]+'">'+val["電話"]+'</a>'+
 					'	</p>'+
@@ -101,7 +100,7 @@ var app = {
 			
 			$.each(data, function(key, val) {
 				$("#nightmarket").append(
-					'<li><a href="'+val["地圖"]+'" target="_blank">'+val["區別"]+' - '+val["夜市名"]+'</a></li>'
+					'<li id="nightmarket-'+key+'"><a href="'+val["地圖"]+'" target="_blank">'+val["夜市名"]+'</a></li>'
 				)
 				if(key == data.length - 1) {
 					$("#nightmarket").listview("refresh");
@@ -118,7 +117,7 @@ var app = {
 			$.each(data, function(key, val) {
 				$("#interestingPlace").append(
 					'<div data-role="collapsible">'+
-					'	<h3>'+val["個案名稱"]+'</h3>'+
+					'	<h3 id="interestingPlace-'+key+'">'+val["個案名稱"]+'</h3>'+
 					'	<p>種類級別：'+val["種類"]+' '+val["級別"]+'<br />'+
 					'		地理位置：<a href="http://maps.google.com/?q='+val["所在地理區域-縣市"]+val["所在地理區域-鄉鎮市"]+val["地址或位置"]+'">'+val["所在地理區域-縣市"]+val["所在地理區域-鄉鎮市"]+val["地址或位置"]+'</a>'+
 					'		歷史沿革：<br />　　'+val["歷史沿革"]+
@@ -137,7 +136,7 @@ var app = {
 			
 			$.each(data, function(key, val) {
 				$("#temple").append(
-					'<li><a href="http://maps.google.com/?q='+val["地址"]+'" target="_blank">'+val["行政區"]+' - '+val["寺廟名稱"]+'</a></li>'
+					'<li id="interestingPlace-'+key+'"><a href="http://maps.google.com/?q='+val["地址"]+'" target="_blank">'+val["寺廟名稱"]+'</a></li>'
 				)
 				if(key == data.length - 1) {
 					$("#temple").listview("refresh");
@@ -147,15 +146,15 @@ var app = {
 	},
 	service: {
 		toilet: function (data) {
-			var data = data || window["toilet"];
+			var data = data || window["toilet"].result.records;
 			$("#main").append('<ul id="toilet" data-role="listview" data-filter="true" data-input="#search" data-inset="true"></ul>');
 			$("#toilet").listview();
 			
-			$.each(data.result.records, function(key, val) {
+			$.each(data, function(key, val) {
 				$("#toilet").append(
-					'<li><a href="http://maps.google.com/?q='+val["地址或地點描述"]+'" target="_blank">'+val["最新公廁級別"]+' - '+val["公廁名稱"]+'</a></li>'// +val["縣市名稱"]+'+'+val["鄉鎮名稱"]+'+'+val["村里名稱"]+'+'
+					'<li id="toilet-'+key+'"><a href="http://maps.google.com/?q='+val["地址或地點描述"]+'" target="_blank">'+val["公廁名稱"]+'</a></li>'// +val["縣市名稱"]+'+'+val["鄉鎮名稱"]+'+'+val["村里名稱"]+'+'
 				)
-				if(key == data.result.records.length - 1) {
+				if(key == data.length - 1) {
 					$("#toilet").listview("refresh");
 				}
 			})
@@ -167,7 +166,7 @@ var app = {
 			
 			$.each(data, function(key, val) {
 				$("#wifi").append(
-					'<li><a href="http://maps.google.com/?q='+val["熱點地址"]+'" target="_blank">'+val["台南市地區"]+' - '+val["臺南市無線網路熱點名稱"]+'</a></li>'
+					'<li id="wifi-'+key+'"><a href="http://maps.google.com/?q='+val["熱點地址"]+'" target="_blank">'+val["臺南市無線網路熱點名稱"]+'</a></li>'
 				)
 				if(key == data.length - 1) {
 					$("#wifi").listview("refresh");
